145c94dc3798b40c06647585628a44a829844b45
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2025-03-25 21:07:02 +0100

    feat: scrap second site

--- scrap.py
+++ scrap.py
@@ -7,19 +7,20 @@ from bs4 import BeautifulSoup
 import time
 import os
 
-fileName = "proverbsdbEnglish.txt"
+fileName = "theenglishdigest.txt"
 
 # Preventive to do not scrap twice the website above
 if os.path.exists( f"./{fileName}"):
     print(f"File exists : {fileName}")
     exit(1)
-# Récupération du contenu HTML de la page web
-base_url = "https://www.proverbsdb.com/proverb-nation/991880/english-proverb"
+
+# Change this url to change website
+base_url = "https://theenglishdigest.com/a-list-of-1000-proverbs-in-english-with-their-meaning/"
 page = requests.get(base_url)
 soup = BeautifulSoup(page.content, "html.parser")
 
 # Recherche des proverbes 
-proverb_elts = soup.find_all("span", class_="text")
+proverb_elts = soup.find_all("h4")
 
 # Use mode 'a' to append to the database
 with open(fileName, "a") as f:
@@ -30,10 +31,9 @@ with open(fileName, "a") as f:
     f.close()
 
 
-page: int = 2
 
-
-for page in range(2, 101):
+# If website contains multiple pages following the same patern
+for page in range(2, 1):
     print(f"ON PAGE : {page}\n")
     url = base_url + f"/{page}"
     try:
