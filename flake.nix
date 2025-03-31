4cf6b6b42d64c196b6c60971c6c8a190ecb86cd4
Louis Thevenet <louis.tvnt@gmail.com>
2025-03-31 15:56:08 +0200

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem =
+        {
+
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+
+            packages = with pkgs; [
+              # Nix
+              nil
+              alejandra
+
+              # Typst
+              typst
+              tinymist
+              typstyle
+
+              (pkgs.python3.withPackages (
+                python-pkgs: with python-pkgs; [
+                  requests
+                  beautifulsoup4
+                ]
+              ))
+            ];
+          };
+        };
+    };
+}
